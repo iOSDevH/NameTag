d7edf1ab4e6a9b5ab007e09a2ca2c18874098705
iOSDevHeath <iOSDevHeath@icloud.com>
2021-12-27 16:24:00 -0500

    Initial Commit

---
+++ NameTag/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  NameTag
+//
+//  Created by Heath Fashina on 2021-12-27.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ NameTag/NameTagApp.swift
@@ -0,0 +1,17 @@
+//
+//  NameTagApp.swift
+//  NameTag
+//
+//  Created by Heath Fashina on 2021-12-27.
+//
+
+import SwiftUI
+
+@main
+struct NameTagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
